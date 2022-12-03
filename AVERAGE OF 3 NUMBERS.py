8b12aa1ec29522afafa6c9a8201fdc6b27bb1573
Toddlesplash001 <118872908+Toddlesplash001@users.noreply.github.com>
2022-12-03 17:58:41 +0530

    Add files via upload

---
+++ AVERAGE OF 3 NUMBERS.py
@@ -0,0 +1,6 @@
+a = int ( input("First integer\n"))
+b = int ( input("Second integer\n"))
+c = int ( input("Third integer\n"))
+d = (a+b+c)/3
+print ("The average of the three numbers is\n")
+print (d)
